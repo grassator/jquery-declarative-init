01fd860c55f63b3a74b19b34b18a20c9a40f3c18
Dmitriy Kubyshkin <dmitriy@kubyshkin.ru>
2013-08-29 22:02:16 +0700

    Fixed global variable leaks and added better error handling

--- jquery.declarative-init.js
+++ jquery.declarative-init.js
@@ -1,19 +1,21 @@
-  /*!
-   * Declarative Init - jQuery Plugin
-   * Provides a way to init jquery plugins declaratively.
-   * 
-   * © 2012 Dmitriy Kubyshkin (http://kubyshkin.ru)
-   * 
-   * Version: 1.0
-   * Requires: jQuery v1.5+
-   *
-   * Dual licensed under the MIT and GPL licenses:
-   *   http://www.opensource.org/licenses/mit-license.php
-   *   http://www.gnu.org/licenses/gpl.html
-   */
+/*!
+ * Declarative Init - jQuery Plugin
+ * Provides a way to init jquery plugins declaratively.
+ * 
+ * © 2012-2013 Dmitriy Kubyshkin (http://kubyshkin.ru)
+ * 
+ * Version: 1.0.1
+ * Requires: jQuery v1.5+
+ *
+ * Dual licensed under the MIT and GPL licenses:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *   http://www.gnu.org/licenses/gpl.html
+ */
+
+/*global jQuery, Zepto*/
 (function($) {
   $.fn.declarativeInit = function(options) {
-    var options = $.extend({
+    options = $.extend({
       pluginDefaults: {},
       pluginGetter: function(element) {
         // If there's no onclick there's nothing to initialize
@@ -33,8 +35,13 @@
       if (!plugins) return;
 
       // We just iterate over the plugins and initialize them
-      for (key in plugins) {
-        $(this)[key]($.extend({}, options.pluginDefaults[key], plugins[key]));
+      for (var key in plugins) {
+        // Nicer error message 
+        if(!$.fn[key] && console && console.error) {
+          console.error("Plugin '" + key + "' not found while initializing");
+        } else {
+          $(this)[key]($.extend({}, options.pluginDefaults[key], plugins[key]));
+        }
       }
     });
   };
